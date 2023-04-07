76bbbc564f13dab18f1c9138be78978fa2f6e0f4
Behruz <95170761+Behruz111@users.noreply.github.com>
2023-04-07 18:03:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from fastai.vision.all import *
 import pathlib
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
 import plotly.express as px
-
+import platform 
+plt = platform.system()
+if plt = 'Linux':pathlib.WindowsPath=pathlib.PosixPath
 
 st.title("Transpoprt")
 
